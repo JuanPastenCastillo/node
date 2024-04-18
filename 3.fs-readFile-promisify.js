c249c109750a84cee17e3762b59386b15d630757
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2024-04-18 14:58:48 -0400

    Added - Readfile: sync; callback, promisify

---
+++ 3.fs-readFile-promisify.js
@@ -0,0 +1,23 @@
+/*
+ * Use this only in native modules who doesn't support promises
+ */
+const fs = require("node:fs")
+const { promisify } = require("node:util")
+
+const readFilePromisify = promisify(fs.readFile)
+
+const {
+  allCSSConsoleLog_CJS,
+} = require("./css_console_log/cjs/allCSSConsoleLog_CJS.js")
+
+console.log("⌛ Reading first file...")
+readFilePromisify("./text.txt", "utf-8").then((text) => {
+  console.log(allCSSConsoleLog_CJS.FgCyan, "text:", text)
+})
+
+console.log("❤ Make things while the files is readed")
+
+console.log("✨Reading second file...")
+readFilePromisify("./text2.txt", "utf-8").then((text) => {
+  console.log(allCSSConsoleLog_CJS.FgMagenta, "text:", text)
+})
---
+++ 3.fs-readFileSync.js
@@ -0,0 +1,11 @@
+const fs = require("node:fs")
+
+console.log("⌛reading first file...")
+const text = fs.readFileSync("./text.txt", "utf-8")
+console.log("text:", text)
+
+console.log("❤Make things while the files is readed")
+
+console.log("✨reading second file...")
+const text2 = fs.readFileSync("./text2.txt", "utf-8")
+console.log("text2:", text2)
