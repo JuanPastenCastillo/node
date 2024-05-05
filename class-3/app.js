2ea7e570e082aa77c460ba84451c23eb6b9dabb2
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2024-05-05 14:51:54 -0400

    Added - Some hint on PORT for express API

--- class-3/app.js
+++ class-3/app.js
@@ -328,6 +328,7 @@ app.options(`${ROUTES.MOVIES}/:id`, (req, res) => {
 })
 */
 
+/* This way to declare the PORT is important to deploy it on some service */
 const PORT = process.env.PORT ?? 3000
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`)
